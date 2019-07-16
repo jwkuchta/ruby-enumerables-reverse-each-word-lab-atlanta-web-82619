d180982009e3fbb1708d78c739aaa16dd1409f97
Justyna Kuchta <justyna.kuchta@gmail.com>
2019-07-16 20:52:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
-reverse_each_word = ("Hello there, and how are you?")
-array_of_words = []
-array_of_words.push(reverse_each_word)
-
-%w[array_of_words].each do |word|
-  word.reverse
+def reverse_each_word(sentence)
+  array_of_words = []
+  array_of_words.push(sentence)
+  %w(array_of_words).each do |word|
+    word.reverses
+  end
+  array_of_words
 end
+
