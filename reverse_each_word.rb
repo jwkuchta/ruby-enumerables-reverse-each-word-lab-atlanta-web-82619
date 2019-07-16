8f3329b35f36e8384cd540b98dc96cd7da8c9101
Justyna Kuchta <justyna.kuchta@gmail.com>
2019-07-16 21:03:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,11 @@ words_array = mine.split(/ /)
 p words_array
 
 def reverse_each_word(sentence)
-  array_of_words = []
-  array_of_words.push(sentence)
-  array_of_words.split()
+  array_of_words = sentence.split(/ /)
+  array_of_words.each do |word|
+    word.reverse!
+  end
+  array_of_words
 end
 
 #puts %w(array_of_words)
