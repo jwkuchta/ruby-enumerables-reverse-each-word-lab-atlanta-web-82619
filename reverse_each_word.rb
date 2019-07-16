cb546ce0b065dc36916db36798963ff3d8ad8d16
Justyna Kuchta <justyna.kuchta@gmail.com>
2019-07-16 21:06:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,13 @@ mine = "I am a happy little sentence"
 
 
 def reverse_each_word(sentence)
+  reversed = []
   array_of_words = sentence.split(/ /)
   array_of_words.each do |word|
     word.reverse!
+    reversed.push(word)
   end
-  array_of_words
+  reversed
 end
 
 puts reverse_each_word(mine)
